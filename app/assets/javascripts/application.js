45c51c2a4c370f30046c2bd7ae2415dc8e1a2b6e
Chalo <gonzalogarcia93@gmail.com>
2019-01-08 11:44:25 -0300

    Add reservation's editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,12 @@
 //= require 'angular/angular'
 //= require 'angular-route/angular-route'
 //= require 'angular-resource/angular-resource'
-//= require angular-animate/angular-animate
-//= require angular-aria/angular-aria
-//= require angular-material/angular-material
+//= require 'angular-animate/angular-animate'
+//= require 'angular-aria/angular-aria'
+//= require 'angular-material/angular-material'
+
+//= require 'angular-sanitize/angular-sanitize'
+//= require 'quill/dist/quill'
+//= require 'ng-quill/dist/ng-quill'
 
 //= require 'moment/moment'
